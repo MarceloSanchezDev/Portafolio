f574919e4b0446f26d6a1d7962fa9b03b8b25d15
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-01-31 20:27:56 -0300

    fix anything

--- api/index.js
+++ api/index.js
@@ -8,12 +8,18 @@ export default async function handler(req, res) {
   const { nombre, email, mensaje } = req.body; // Cambiamos los nombres
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    tls: {
+      rejectUnauthorized: false,
+    },
   });
+  
 
   const mailOptions = {
     from: process.env.EMAIL_USER,
