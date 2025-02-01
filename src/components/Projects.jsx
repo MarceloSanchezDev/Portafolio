1644f41f4d6cd4978c414f47078530600916c7c0
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-02-01 17:27:00 -0300

    fix heigth de projectos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,39 @@
 import { Project } from "./Project";
+import appPeliculas from "../assets/appPeliculas.png";
 export function Projects() {
   return (
     <div>
       <div className="projectos">
         <h1 className="tituloSeconSeccion ">🌐 Projectos</h1>
-        <Project></Project>
-        <Project></Project>
-        <Project></Project>
-        <Project></Project>
+        <Project
+          titulo="App de Peliculas 🍿🎬 "
+          img={appPeliculas}
+          descript="Página de Películas Una aplicación web para explorar, buscar y gestionar películas favoritas. Utiliza la API de TMDb para obtener datos de películas y cuenta con un sistema de autenticación desarrollado en Node.js. Frontend creado con React y estilizado con Bootstrap."
+          url="https://pagina-de-peliculas.onrender.com/"
+          tec={["React.js", "Node.js", "Bootstrap"]}
+        ></Project>
+        <Project
+          titulo="App de Estadisticas 📊📈"
+          img={appPeliculas}
+          descript="Aplicación web para visualizar estadísticas de baloncesto, enfocada en tiros de dos y tres puntos. Permite ver el rendimiento de los jugadores, con detalles sobre aciertos y fallos. La estructura del proyecto está dividida en workspaces para el backend y frontend.
+Utiliza React en el frontend y Node.js con Express en el backend. Implementa autenticación con JWT y protección de contraseñas con Bcrypt. Usa MySQL para gestionar los datos y Zod para validación."
+          url="https://estadisticas-app.onrender.com/"
+          tec={["React.js", "Node.js", "MySQL ", "Bootstrap"]}
+        ></Project>
+        <Project
+          titulo="App de Peliculas"
+          img={appPeliculas}
+          descript="Una App de Peliculas"
+          url="https://pagina-de-peliculas.onrender.com/"
+          tec={["React.js", "Node.js", "Bootstrap"]}
+        ></Project>
+        <Project
+          titulo="App de Peliculas"
+          img={appPeliculas}
+          descript="Una App de Peliculas"
+          url="https://pagina-de-peliculas.onrender.com/"
+          tec={["React.js", "Node.js", "Bootstrap"]}
+        ></Project>
       </div>
     </div>
   );
