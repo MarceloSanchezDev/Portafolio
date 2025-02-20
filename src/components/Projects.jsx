0741e519af59bdd06b63a69017d0c3a2d78efeb5
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-02-20 10:29:38 -0300

    add new photos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,7 @@
 import { Project } from "./Project";
-import appPeliculas from "../assets/appPeliculas.png";
+import appPeliculas2 from "../assets/AppPeliculasDetails.png";
+import appPeliculas3 from "../assets/AppPeliculasList.png";
+import appPeliculas4 from "../assets/AppPeliculasRes.png";
 export function Projects() {
   return (
     <div>
@@ -7,14 +9,14 @@ export function Projects() {
         <h1 className="tituloSeconSeccion ">🌐 Projectos</h1>
         <Project
           titulo="App Web  de Peliculas 🍿🎬 "
-          img={appPeliculas}
+          img={[appPeliculas2, appPeliculas3, appPeliculas4]}
           descript="Una aplicación web para explorar, buscar y gestionar películas favoritas. Utiliza la API de TMDb para obtener datos de películas y cuenta con un sistema de autenticación desarrollado en Node.js. Frontend creado con React y estilizado con Bootstrap."
           url="https://peliculas-murex.vercel.app/"
           tec={["React.js", "Node.js", "MySQL", "Bootstrap"]}
         ></Project>
         <Project
           titulo="App Web de Estadisticas 📊📈"
-          img={appPeliculas}
+          img={[appPeliculas2, appPeliculas3, appPeliculas4]}
           descript="Aplicación web para visualizar estadísticas de baloncesto, enfocada en tiros de dos y tres puntos. Permite ver el rendimiento de los jugadores, con detalles sobre aciertos y fallos. La estructura del proyecto está dividida en workspaces para el backend y frontend.
 Utiliza React en el frontend y Node.js con Express en el backend. Implementa autenticación con JWT y protección de contraseñas con Bcrypt. Usa MySQL para gestionar los datos y Zod para validación."
           url="https://estadisticas-app.onrender.com/"
@@ -22,14 +24,14 @@ Utiliza React en el frontend y Node.js con Express en el backend. Implementa aut
         ></Project>
         <Project
           titulo="App de Peliculas"
-          img={appPeliculas}
+          img={[appPeliculas2, appPeliculas3, appPeliculas4]}
           descript="Una App de Peliculas"
           url="https://pagina-de-peliculas.onrender.com/"
           tec={["React.js", "Node.js", "Bootstrap"]}
         ></Project>
         <Project
           titulo="App de Peliculas"
-          img={appPeliculas}
+          img={[appPeliculas2, appPeliculas3, appPeliculas4]}
           descript="Una App de Peliculas"
           url="https://pagina-de-peliculas.onrender.com/"
           tec={["React.js", "Node.js", "Bootstrap"]}
